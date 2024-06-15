b8882832653dc0c6b817115f33ba0d71e02eaa33
natishget <natishget@gmail.com>
2024-06-15 15:50:30 +0300

    blog changed

--- src/data/BlogTitles.js
+++ src/data/BlogTitles.js
@@ -1,7 +1,17 @@
-import img1 from '../assets/images/blogs/kirkos/1.webp'
-import img2 from '../assets/images/blogs/bezalel/1.webp'
+import img1 from '../assets/images/blogs/newProduct/laserCutting.jpg'
+import img2 from '../assets/images/blogs/newProduct/UV.jpg'
 
 export default[
-    {id: 1, comp: 'kir', title: 'Full Branding', image: img1, style: 'w-full md:h-96 normshad rounded-xl flex flex-col md:grid grid-cols-3 shadow-lg overflow-hidden hover:shadow-2xl md:transBox cursor-pointer', name: 'Kirkos Subcity', delay: 100, desc: 'Full Branding Done for Kirkos Sub-city including Front Table Design, 3D light box for rooms, meeting rooms design, brouchers and many more...'},
-    {id: 2, comp: 'bez', title: 'Full Branding', image: img2, style: 'w-full md:h-96 normshad rounded-xl flex flex-col md:grid grid-cols-3 shadow-lg overflow-hidden hover:shadow-2xl md:transBox cursor-pointer', name: 'Bezalel Aluminium', delay: 200,  desc: 'Full Branding for Bezalel Aluminium including meeting room design, brouchers, Bussiness Cards, Custom Pens, Fliers...'}
+    {id: 1, comp: 'kir', title: 'New CO2 Laser Cutting Machine', image: img1, style:
+        'w-full md:h-96 normshad rounded-xl flex flex-col md:grid grid-cols-3 shadow-lg overflow-hidden hover:shadow-2xl md:transBox cursor-pointer',
+        name: 'Laser Cutting Machine',
+        delay: 100,
+        desc: 'A new CO2 Laser Cutting machine used for Cutting, Engraving, Tracing an Acrlyric material'
+    },
+    {id: 2, comp: 'bez', title: ' New UV Printing mAchine ', image: img2, style:
+        'w-full md:h-96 normshad rounded-xl flex flex-col md:grid grid-cols-3 shadow-lg overflow-hidden hover:shadow-2xl md:transBox cursor-pointer',
+        name: 'CMYK Flatbed UV Printing Machine',
+        delay: 200,
+        desc: 'UV flatbed printer is a high precision digital printing machine designed for personalized products'
+    }
 ]
